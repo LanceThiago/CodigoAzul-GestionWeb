77e8a94d5344ffafbeefd5d033c2d7b785075492
Eneas Cisneros <101111243+CisnerosEneas@users.noreply.github.com>
2022-11-18 12:41:04 -0300

    Conexion con host

--- DB/DBConnection.php
+++ DB/DBConnection.php
@@ -1,5 +1,5 @@
 <?php
-  function DbHandler  () {
+  /*function DbHandler  () {
     $dbHost = 'localhost';
     $dbName = 'codigo_azul';
     $dbUser = 'root';
@@ -16,5 +16,23 @@
     } catch (Exception $e) {
       var_dump('Couldn\'t Establish A Database Connection. Due to the following reason: ' . $e->getMessage());
     }
-  }
+  }*/
+  function DbHandler  () {
+      $dbHost = 'sql200.epizy.com';
+      $dbName = 'epiz_33009566_codigo_azul';
+      $dbUser = 'epiz_33009566';
+      $dbPass = 'vqTJxDGT0o2';
+      $Dsn = "mysql:host=" . $dbHost . ";dbname=" . $dbName;
+      $options = array(
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+      );
+
+      try {
+        $Connection = new PDO($Dsn, $dbUser, $dbPass, $options);
+        return $Connection;
+      } catch (Exception $e) {
+        var_dump('Couldn\'t Establish A Database Connection. Due to the following reason: ' . $e->getMessage());
+      }
+    }
 ?>
